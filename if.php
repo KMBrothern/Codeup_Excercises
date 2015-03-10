d385ba1911ae9f29779e2fec80c9fbc940cef647
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-10 11:33:02 -0500

    using if elseif else blocks and changing values with operators in php script

--- if.php
+++ if.php
@@ -1,48 +1,47 @@
 <?php
 
-$a = 5;
- $b = 10;
- $c = '10';
+$a = 7;
+ $b = 12;
+ $c = '12';
 
 if ($a < $b) {
      // output the appropriate result
      echo "$a is less than $b\n";
- }
+ }else{
 
- if ($b > $a) {
-     // output the appropriate result
      echo "$b is greater than $a\n";
+ 
  }
-
+// end of if else stamement 1
  if ($b >= $c) {
      // output the appropriate result
      echo "$b is greater than or equal to $c\n";
- }
-
- if ($b <= $c) {
+ }else{
      // output the appropriate result
      echo "$b is less than or equal to $c\n";
+ 
  }
+ // end of if else statement 2
 
- if ($b == $c) {
-     // output the appropriate result
-     echo "$b is equal to $c\n";
- }
+ 
 
  if ($b === $c) {
-     // output the appropriate result
      echo "$b is identical $c\n";
- }
 
- // output the appropriate result
- if ($b !== $c) {
-     echo "$b is not equal to $c\n";
- }
-
- // output the appropriate result
- if ($b !== $c) {
+ }else if ($b !== $c) {
      echo "$b is not identical to $c\n";
- }
+
+}elseif ($b == $c) {
+     echo "$b is equal to $c\n";
+}else{
+      echo "$b is not equal to $c\n";
+
+}
+
+ 
+
+ 
+
 
 
 
