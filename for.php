4e35be7bf79ba4a558da39339ce2032728cf5bad
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-11 13:03:17 -0500

    foreaach is_array results,as well as changes to my fizzbuzz and for files

--- for.php
+++ for.php
@@ -4,8 +4,9 @@ fwrite(STDOUT, 'Please give me a starting number ');
 $startingNumber = trim(fgets(STDIN));
 fwrite(STDOUT, 'Please give me an ending number ');
 $endingNumber = trim(fgets(STDIN));
-fwrite(STDOUT, 'Please choose your increment - $i += number you\'d like to increase by ');
+fwrite(STDOUT, 'Please choose the number you\'d like to count by ');
 $incrementValue = trim(fgets(STDIN));
+
 for ($i = $startingNumber; $i <= $endingNumber; $i+= $incrementValue){
 	echo "$i\n";
 }
