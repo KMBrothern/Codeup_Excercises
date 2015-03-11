0cbe9ea887c08022fbe0607e1bcabe2ec3b3d744
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-11 10:17:37 -0500

    builds on step 1, now user can also choose their increment value as well, using for loop for display

--- for.php
+++ for.php
@@ -4,6 +4,8 @@ fwrite(STDOUT, 'Please give me a starting number ');
 $startingNumber = trim(fgets(STDIN));
 fwrite(STDOUT, 'Please give me an ending number ');
 $endingNumber = trim(fgets(STDIN));
-for ($i = $startingNumber; $i <= $endingNumber; $i++){
+fwrite(STDOUT, 'Please choose your increment - $i += number you\'d like to increase by ');
+$incrementValue = trim(fgets(STDIN));
+for ($i = $startingNumber; $i <= $endingNumber; $i+= $incrementValue){
 	echo "$i\n";
 }
